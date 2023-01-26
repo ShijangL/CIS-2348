00acabeba28ebf65fa556a412f12460c4c2e3cff
ShijangL <123122751+ShijangL@users.noreply.github.com>
2023-01-26 12:05:58 -0600

    Add files via upload
    Python file for 1.12 zyLab

---
+++ 1.12 Lab.py
@@ -0,0 +1,4 @@
+userNum = int(input('Enter a number to be squared: '))
+userNumSquared = userNum * userNum  # Bug here; fix it when instructed
+
+print(userNumSquared, end=' ')  # Output formatting issue here; fix it when instructed
