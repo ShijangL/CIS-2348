6d147d07908b50f64b7e6b36395faa6058436bbb
ShijangL <123122751+ShijangL@users.noreply.github.com>
2023-01-26 12:25:47 -0600

    1.20 zyLab
    zyLab for section 1.20

---
+++ main.py
@@ -0,0 +1,11 @@
+print('Enter integer:')
+user_num1 = int(input())
+
+print(f'You entered: {user_num1}')
+print(f'{user_num1} squared is {user_num1 * user_num1}')
+print(f'And {user_num1} cubed is {user_num1 ** 3} !!')
+
+print('Enter another integer:')
+user_num2 = int(input())
+print(f'{user_num1} + {user_num2} is {user_num1 + user_num2}')
+print(f'{user_num1} * {user_num2} is {user_num1 * user_num2}')
