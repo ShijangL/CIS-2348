30278dedbfb70fa56392f6934d3cee8cde9ef344
ShijangL <shijang.lin28@gmail.com>
2023-02-17 14:20:26 -0600

    Finding the answer

--- Homework #2/ZyLab 6.22.py
+++ Homework #2/ZyLab 6.22.py
@@ -12,4 +12,8 @@ answer_founded = False
 
 for x in range(-10, 11):
     for y in range(-10, 11):
-
+        if (x1 * x + y1 * y == total1) and (x2 * x + y2 * y == total2):
+            answer_founded = True
+            print(f'{x} {y}')
+    if not answer_founded:
+        print('No solution')
