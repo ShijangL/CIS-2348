d70541d14353484b1831c7f2cb82566dc0764ebb
ShijangL <shijang.lin28@gmail.com>
2023-02-17 14:19:37 -0600

    Setting the Range

--- Homework #2/ZyLab 6.22.py
+++ Homework #2/ZyLab 6.22.py
@@ -1,6 +1,6 @@
 # Shijang Lin PSID: 2018904
 
-#Lab 6.22
+# Lab 6.22
 
 x1 = int(input())
 y1 = int(input())
@@ -8,6 +8,8 @@ total1 = int(input())
 x2 = int(input())
 y2 = int(input())
 total2 = int(input())
+answer_founded = False
 
-for x in range(-10,11):
+for x in range(-10, 11):
+    for y in range(-10, 11):
 
