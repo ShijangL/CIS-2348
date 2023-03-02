1d3e71bd1cba15c22b6fa779792205ff0f4b98a4
ShijangL <shijang.lin28@gmail.com>
2023-03-02 12:01:40 -0600

    function

--- Homework #2/ZyLab 7.25.py
+++ Homework #2/ZyLab 7.25.py
@@ -1,3 +1,15 @@
 # Shijang Lin PSID: 2018904
 
-#Lab 7.25
+# Lab 7.25
+
+def exact_change(user_total):
+    dollars = user_total // 100
+    remainder = user_total % 100
+    quarter = remainder // 25
+    remainder = user_total % 25
+    dime = remainder // 10
+    remainder = remainder % 10
+    nickel = remainder // 5
+    penny = remainder % 5
+
+    return dollars, quarter, dime, nickel, penny
