7cbce51778831fdfc198374787cde0b4edbf8c40
ShijangL <shijang.lin28@gmail.com>
2023-03-02 12:16:26 -0600

    Palindrome Function

--- Homework #2/zyLab 8.10.py
+++ Homework #2/zyLab 8.10.py
@@ -1,3 +1,13 @@
 # Shijang Lin PSID: 2018904
 
-#Lab 8.10
+# Lab 8.10
+
+def is_palindrome(word):
+    new_word = word.replace(' ', '')
+    reverse = ''
+    result = False
+    for i in new_word:
+        reverse = i + reverse
+    if reverse == new_word:
+        result = True
+    return result
