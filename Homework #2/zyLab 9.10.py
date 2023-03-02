efc4c42d5d0087cf056146b6d5be5d6fa01f0ef1
ShijangL <shijang.lin28@gmail.com>
2023-03-02 13:20:49 -0600

    Output

--- Homework #2/zyLab 9.10.py
+++ Homework #2/zyLab 9.10.py
@@ -1,3 +1,19 @@
 # Shijang Lin PSID: 2018904
 
 # Lab 9.10
+
+import csv
+
+new_dict = {}
+filename = input()
+with open(filename, 'r') as myfile:
+    aline = list(csv.reader(myfile, delimiter=','))
+
+    for i in aline[0]:
+        if new_dict.get(i, 0) == 0:
+            new_dict[i] = 1
+        else:
+            new_dict[i] += 1
+
+for k in new_dict:
+    print(f'{k} {new_dict[k]}')
