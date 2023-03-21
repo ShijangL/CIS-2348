502bc7bd3f19816072613e9b5c3f8162925990e9
ShijangL <shijang.lin28@gmail.com>
2023-03-21 16:58:42 -0500

    End

--- Homework #3/zyLab 10.15.py
+++ Homework #3/zyLab 10.15.py
@@ -7,3 +7,23 @@ class Team:
         self.team_name = 'none'
         self.team_wins = 0
         self.team_losses = 0
+
+    def get_win_percentage(self):
+        return self.team_wins / (self.team_wins + self.team_losses)
+
+
+if __name__ == "__main__":
+    name = input()
+    wins = int(input())
+    losses = int(input())
+
+    Team_Results = Team()
+    Team_Results.team_name = name
+    Team_Results.team_wins = wins
+    Team_Results.team_losses = losses
+
+    average = Team_Results.get_win_percentage()
+    if average > .5:
+        print(f'Congratulations, Team {name} has a winning average!')
+    else:
+        print(f'Team {name} has a losing average.')
