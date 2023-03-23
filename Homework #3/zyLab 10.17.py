3729062e21d067a302d9155eb2c437a462ad1dec
ShijangL <shijang.lin28@gmail.com>
2023-03-23 12:15:43 -0500

    Start

--- Homework #3/zyLab 10.17.py
+++ Homework #3/zyLab 10.17.py
@@ -1,3 +1,15 @@
 # Shijang Lin PSID: 2018904
 
 # zyLab 10.17
+
+class ItemToPurchase:
+
+    def __init__(self, name="none", price=0, quantity=0):
+        self.item_name = name
+        self.item_price = price
+        self.item_quantity = quantity
+
+    def print_item_cost(self):
+        print(
+            f'{self.item_name} {self.item_quantity} @ ${self.item_price:.0f} = $'
+            f'{self.item_price * self.item_quantity:.0f}')
