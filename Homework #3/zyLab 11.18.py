238e22c52c9eac97c50267dea14a0c3ab6aa5f81
ShijangL <shijang.lin28@gmail.com>
2023-03-28 12:03:53 -0500

    Sorted List

--- Homework #3/zyLab 11.18.py
+++ Homework #3/zyLab 11.18.py
@@ -1,3 +1,16 @@
 # Shijang Lin PSID: 2018904
 
 # zyLab 11.18
+
+string = input()
+intlist = string.split()
+
+newlist = []
+
+for i in intlist:
+    if int(i) >= 0:
+        newlist.append(int(i))
+
+newlist.sort()
+for j in newlist:
+    print(j, end=' ')
