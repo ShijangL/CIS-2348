6ca9a4906d84c5176ec8ac631cb02fac3cfac99e
ShijangL <shijang.lin28@gmail.com>
2023-03-28 14:46:30 -0500

    Part 1

--- Homework #3/zyLab 11.27.py
+++ Homework #3/zyLab 11.27.py
@@ -6,3 +6,17 @@ from os import system
 
 system('cls')
 
+new_dict = {}
+
+for num in range(5):
+    jnum = input(f"Enter player {num + 1}'s jersey number:\n")
+    prate = input(f"Enter player {num + 1}'s rating:\n")
+
+    if jnum not in new_dict:
+        new_dict[int(jnum)] = int(prate)
+
+    print()
+
+print("ROSTER")
+for k in sorted(new_dict):
+    print(f'Jersey number: {k}, Rating: {new_dict[k]}')
