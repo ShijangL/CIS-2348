b18fcf537c416d05c859b3faabc400f71649ea04
ShijangL <shijang.lin28@gmail.com>
2023-04-01 16:36:37 -0500

    Opening the Files

--- FinalProjectPart1/FinalProject.py
+++ FinalProjectPart1/FinalProject.py
@@ -1,3 +1,33 @@
 # Shijang Lin PSID: 2018904
 
 # Final Project Part 1
+
+from os import system
+import csv
+import datetime
+
+system('cls')
+
+Mlist = []
+Plist = []
+Slist = []
+
+# Copy the datas from the files into a list that can be scriptable
+
+with open('ManufacturerList.csv', 'r') as ManufacturerList:
+    ManuList = csv.reader(ManufacturerList, delimiter=',')
+
+    for m in ManuList:
+        Mlist.append(m)
+
+with open('PriceList.csv', 'r') as PriceList:
+    PrList = csv.reader(PriceList, delimiter=',')
+
+    for p in PrList:
+        Plist.append(p)
+
+with open('ServiceDatesList.csv', 'r') as ServiceList:
+    SDateList = csv.reader(ServiceList, delimiter=',')
+
+    for s in SDateList:
+        Slist.append(s)
