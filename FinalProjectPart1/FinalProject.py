181ca5fad55fbda6011948a5ba8267c21ce24499
ShijangL <shijang.lin28@gmail.com>
2023-04-01 16:38:38 -0500

    Part 1

--- FinalProjectPart1/FinalProject.py
+++ FinalProjectPart1/FinalProject.py
@@ -31,3 +31,32 @@ with open('ServiceDatesList.csv', 'r') as ServiceList:
 
     for s in SDateList:
         Slist.append(s)
+
+Manufacturers = []
+for company in Mlist:
+    if company[1] not in Manufacturers:
+        Manufacturers.append(company[1])
+
+Manufacturers.sort()
+
+# Loop to write Full Inventory file
+
+with open('FullInventory.csv', 'w', newline='') as FullInventory:
+    list_writer = csv.writer(FullInventory)
+    for name in Manufacturers:
+        for run in range(len(Mlist)):
+            if name in Mlist[run][1]:
+                ids = Mlist[run][0]
+                for cost in Plist:
+                    if ids in cost[0]:
+                        price = cost[-1]
+                for service in Slist:
+                    if ids in service[0]:
+                        date = service[-1]
+                for model in Mlist:
+                    if ids in model[0]:
+                        types = model[2]
+                        if model[-1] == 'damaged':
+                            list_writer.writerow([ids, name, types, price, date, model[-1]])
+                        else:
+                            list_writer.writerow([ids, name, types, price, date])
