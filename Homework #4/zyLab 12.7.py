4c4c8c411dc051afd8d78c284dc94924eaf88254
ShijangL <shijang.lin28@gmail.com>
2023-04-12 13:54:53 -0500

    End Code

--- Homework #4/zyLab 12.7.py
+++ Homework #4/zyLab 12.7.py
@@ -2,19 +2,29 @@
 
 # zyLab 12.7
 
+from os import system
+
+system('cls')
+
+
 def get_age():
     age = int(input())
-    # TODO: Raise exception for invalid ages
+    if age < 18 or age > 75:
+        raise ValueError('Invalid age.')
     return age
 
 
 # TODO: Complete fat_burning_heart_rate() function
 def fat_burning_heart_rate(age):
-
+    heart_rate = .7 * (220 - age)
     return heart_rate
 
 
 if __name__ == "__main__":
-    # TODO: Modify to call get_age() and fat_burning_heart_rate()
-    #       and handle the exception
-    age = get_age()
+    try:
+        age = get_age()
+        heart = fat_burning_heart_rate(age)
+        print(f'Fat burning heart rate for a {age} year-old: {heart} bpm')
+    except ValueError as excpt:
+        print(excpt)
+        print("Could not calculate heart rate info.\n")
