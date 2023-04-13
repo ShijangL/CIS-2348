a8987956ef983a6bad28730f91379cd4cc7cff66
ShijangL <shijang.lin28@gmail.com>
2023-04-13 12:50:45 -0500

    Ending Code

--- Homework #4/zyLab 12.9.py
+++ Homework #4/zyLab 12.9.py
@@ -2,13 +2,21 @@
 
 # zyLab 12.9
 
+from os import system
+
+system('cls')
+
 # Split input into 2 parts: name and age
 parts = input().split()
 name = parts[0]
 while name != '-1':
     # FIXME: The following line will throw ValueError exception.
     #        Insert try/except blocks to catch the exception.
-    age = int(parts[1]) + 1
+    try:
+        age = int(parts[1]) + 1
+    except ValueError:
+        age = 0
+
     print(f'{name} {age}')
 
     # Get next line
