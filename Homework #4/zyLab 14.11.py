616a81a408c1331da0a5af596455775294bebb72
ShijangL <shijang.lin28@gmail.com>
2023-04-13 14:51:21 -0500

    selection_sort_descend_trace function

--- Homework #4/zyLab 14.11.py
+++ Homework #4/zyLab 14.11.py
@@ -5,3 +5,17 @@
 from os import system
 
 system('cls')
+
+
+def selection_sort_descend_trace(numbers):
+    for i in range(len(numbers) - 1):
+        index_largest = i
+        for j in range(i + 1, len(numbers)):
+            if numbers[j] > numbers[index_largest]:
+                index_largest = j
+
+        numbers[i], numbers[index_largest] = numbers[index_largest], numbers[i]
+
+        for k in numbers:
+            print(k, end=' ')
+        print()
